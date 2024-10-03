5b884b6fb7a61b41035f71f97e380eeacbd8d936
Anna Zhu <annazhu@Annas-MacBook-Pro.local>
2024-10-03 15:50:18 -0400

    Complete basic counter functionality

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,5 +1,13 @@
 import { fromEvent, interval, merge, NEVER } from 'rxjs';
-import { setCount, startButton, pauseButton } from './utilities';
+import { count, setCount, startButton, pauseButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
+let interval$ = interval(1000);
+let subscription;
+
+start$.subscribe(() => {
+    subscription = interval$.subscribe(setCount)
+});
+// can only unsubscribe from a Subscription, not an Observable
+pause$.subscribe(() => subscription.unsubscribe());
