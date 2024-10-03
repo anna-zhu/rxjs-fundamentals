06eb1c2e833c3aaef9b062b30881db2236a1cd2c
Anna Zhu <zeehaychoo@gmail.com>
2024-10-03 18:55:33 -0400

    Manipulating time and merging timelines

--- applications/manipulating-time/script.js
+++ applications/manipulating-time/script.js
@@ -19,6 +19,13 @@ import {
   setStatus,
 } from './utilities';
 
-const buttonClicks$ = fromEvent(button, 'click');
+const panicClicks$ = fromEvent(panicButton, 'click');
+const buttonClicks$ = fromEvent(button, 'click').pipe(
+  // delay(2000) ---> continues to queue
+  // throttleTime(2000) ---> triggers, but starts a rest period set by arg
+  // debounceTime(2000) ---> doesn't trigger until rest period set by arg is reached
+  // throttle(() => panicClicks$) ---> triggers, but cannot be triggered again until arg is executed
+  // debounce(() => panicClicks$) ---> doesn't trigger until arg is executed
+);
 
 buttonClicks$.subscribe(addMessageToDOM);
--- applications/merging-timelines/script.js
+++ applications/merging-timelines/script.js
@@ -1,4 +1,11 @@
-import { fromEvent, merge, interval, concat, race, forkJoin } from 'rxjs';
+import {
+  fromEvent,
+  merge, // whenever triggered, is executed
+  interval,
+  concat, // sequential queue of observables depending on arg sequence
+  race, // only the fastest observable to be emitted
+  forkJoin // returns the last values in the arg observables
+} from 'rxjs';
 import { mapTo, startWith, take, map } from 'rxjs/operators';
 import {
   labelWith,
@@ -7,3 +14,19 @@ import {
   setStatus,
   bootstrap,
 } from './utilities';
+
+const startClicks$ = fromEvent(startButton, 'click').pipe(mapTo(true));
+const pauseClicks$ = fromEvent(pauseButton, 'click').pipe(mapTo(false));
+
+// merge subscribes to the args
+const isRunning$ = merge(startClicks$, pauseClicks$).pipe(startWith(false));
+
+isRunning$.subscribe(setStatus);
+
+const first$ = interval(1000).pipe(map(labelWith('First')), take(4));
+const second$ = interval(1000).pipe(map(labelWith('Second')), take(4));
+// const combined$ = merge(first$, second$);
+// const combined$ = concat(first$, second$);
+// const combined$ = race(first$, second$);
+
+bootstrap({ first$, second$, combined$ });
