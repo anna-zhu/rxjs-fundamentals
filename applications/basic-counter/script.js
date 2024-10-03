81192c09ef05b7e2dbac215eae72053a7d3ac6c5
Anna Zhu <annazhu@Annas-MacBook-Pro.local>
2024-10-03 18:00:35 -0400

    Updating basic counter implementation to use skipUntil, scan, and takeUntil operators

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,13 +1,25 @@
-import { fromEvent, interval, merge, NEVER } from 'rxjs';
-import { count, setCount, startButton, pauseButton } from './utilities';
+import { fromEvent, interval, merge, NEVER, takeUntil, skipUntil, scan } from 'rxjs';
+// import { skipUntil, scan } from 'rxjs/operators';
+import { setCount, startButton, pauseButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
-let interval$ = interval(1000);
-let subscription;
 
-start$.subscribe(() => {
-    subscription = interval$.subscribe(setCount)
-});
-// can only unsubscribe from a Subscription, not an Observable
-pause$.subscribe(() => subscription.unsubscribe());
+// ----- Inital solution (basic counter) -----
+// let timer$ = interval(1000);
+// let subscription;
+
+// start$.subscribe(() => {
+//     subscription = timer$.subscribe(setCount);
+// });
+
+// pause$.subscribe(() => subscription.unsubscribe());
+
+// ----- More optimal solution using skipUntil, scan, takeUntil (combining operators) -----
+const timer$ = interval(1000).pipe(
+  skipUntil(start$),
+  scan(count => count + 1, 0),
+  takeUntil(pause$)  
+);
+
+timer$.subscribe(setCount);
